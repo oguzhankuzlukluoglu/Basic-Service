cc2712bfbd3fa20f36f4a9b76a6a64f17f087def
Oğuzhan <oguzhankuzlukluoglu@gmail.com>
2024-09-24 22:24:28 +0300

    postgres connection performed by using docker

--- service1/go.mod
+++ service1/go.mod
@@ -2,4 +2,7 @@ module github.com/oguzhankuzlukluoglu/service1
 
 go 1.22.1
 
-require github.com/lib/pq v1.10.9
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
+)
--- service1/main.go
+++ service1/main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/oguzhankuzlukluoglu/service1/config"
+)
+
+func main() {
+	// .env dosyasını yükle
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	// DATABASE_URL değişkenini al
+	databaseURL := os.Getenv("DATABASE_URL")
+
+	// Veritabanı bağlantısını kur
+	db, err := config.ConnectToDB(databaseURL)
+	if err != nil {
+		log.Fatalf("Veritabanı bağlantısı başarısız: %v", err)
+	}
+	defer db.Close()
+	fmt.Println("Veritabanına başarıyla bağlandım!")
+}
